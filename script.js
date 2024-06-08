5bfce00ddabb36d404aabf6d4ce1234d0b44e729
Deboranoemy <130715954+Deboranoemy@users.noreply.github.com>
2024-06-07 22:44:24 -0300

    a

---
+++ script.js
@@ -0,0 +1,23 @@
+const caixaPricipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-reultado");
+
+const perguntas = [
+   {
+    enunciado: "Pergunta1", 
+    alternativas: [
+        "alternativa1", 
+        "alternativa2"
+    ]
+     },
+    "Pergunta2"
+    {
+        enunciado: "Pergunta1", 
+        alternativas: [
+            "alternativa1", 
+            "alternativa2"
+        ]
+         },
+]
